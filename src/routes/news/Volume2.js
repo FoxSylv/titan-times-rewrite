21449aeac76798370968e9c93ae3799b422839fe
FoxSylv <atheriault@bowdoin.edu>
2023-12-09 00:03:27 -0500

    Add Volume 2

--- src/routes/news/Volume2.js
+++ src/routes/news/Volume2.js
@@ -1,3 +1,104 @@
 export default function Volume2() {
-    return;
+    return (<>
+        <p>
+            Hello everyone! Welcome back to another release of the Times (this level of consistency in the future? Don’t count on it), the best way to get a quick digest of our recent activities and thoughtful insights on all things Wynn! This one was a lot of fun to put together, so hopefully you’ll enjoy it!
+        </p>
+        <h2>
+            Recent Happenings
+        </h2>
+        <p>
+            Geb hosted a scavenger hunt over in Adonis, in which participants had to search for designated areas around Gavel based on nondescript images. Some of these were really tough (I definitely wasn’t attempting to find each spot even though I couldn’t attend the live event)! Luckily, our own Biblonko, fatfatsheep87, StellarWombat, TheZeldaBoi, Cal, AssassinThugs, and JoeDeez were able to win some prizes, along with some of our ESI friends. Good job to our excellent scavengers!
+        </p>
+        <p>
+            Additionally, Miles hosted a Wynn trivia event in game. Each correct question was worth some LE, so I’m sure it was rather heated! Good work to Snazzy, Geb, Cal, Ugastag, Biblonko, fatfatsheep87, and Aiza who all displayed their knowledge in the game and earned themselves the opportunity to buy themselves something nice.
+        </p>
+        <p>
+            Basically, the cabinets aren’t woven into the hierarchy anymore, so ranking up is based more on activity! You can still join any of the 3 cabinets separately too. Also, the cosmetic roles still exist separately from all other roles, which are based only on the length of time you’ve been a part of the guild. If you have more questions about the new system, ask any member of Council!
+        </p>
+        <p>
+            Now, my favorite part to write: promotions! Not nearly so many as our last recap, but I did have some catching up to do. I’m happy to let you all know (or remind you for those of you who frequent our announcements) that <strong>
+                JoeDeez
+            </strong>, <strong>
+                LoseStreak
+            </strong>, and <strong>
+                Slime1480
+            </strong> have all been promoted to Sorcerer! Many congratulations to them, and I hope to see their names, and others, in this spot soon.
+        </p>
+        <h2>
+            Military Updates
+        </h2>
+        <p>
+            Our beginning-of-season reclaim of our ocean territories went very smoothly. ANOcean lives to see another season! Since then, our FFAers have been busy with frequent struggles against our allies over in DUDE, AVO, TAq, and SDU (others slip in too, but are hardly worth the mention). FOOL has really been making fools of themselves by attempting to attack our claim more than once in the span of a week, but of course failing miserably.
+        </p>
+        <p>
+            An additional reminder that our GXP for LE event will be continuing through the end of this season. This applies to all guild members, not just those who are military-oriented. As of recently, however, the next season of Ocean Trials has begun! For anyone who might not know, Ocean Trials is an incentive to help with wars for LE based on your war count at the end of the season. If you’d like more information, head over to #ocean-trials-info, or ask a Council member for military roles for access to military channels.
+        </p>
+        <center>
+            <img src="https://cdn.discordapp.com/attachments/717490202909737101/978500357611212810/unknown.png" alt="GXP table screenshot" width="66%" />
+            <p>
+                GXP gain in the last 2 weeks
+            </p>
+        </center>
+        <center>
+            <img src="https://cdn.discordapp.com/attachments/717490202909737101/978854979852963901/unknown.png" alt="Example trivia question" width="66%" />
+            <p>
+                Miles' event. Screenshot by Aiza
+            </p>
+        </center>
+        <h2>
+            Main Stage
+        </h2>
+        <p>
+            As most of you likely already know, the Wynncraft dev team recently released the trailer for the upcoming 2.0 update (link <a href="https://youtu.be/UOhzHLdQF1Q">
+                here
+            </a> if you’ve somehow missed it)! There are many exciting things to note in this trailer, including the overhaul of the combat system, a new raid, new quests, and the most talked-about surprise, subclasses and an ability tree! Personally, I can’t wait to see what the new release brings and am very much looking forward to playing around with the changes, but I wanted to see what you thought. I interviewed a good handful of active players to get their thoughts on the trailer, and summarized the most popular thoughts here:
+        </p>
+        <ol>
+            <li>
+                ANO members were unanimous in saying they were looking forward to the 2.0 release. Some were even surprised at how much content was going to be updated.
+            </li>
+            <li>
+                People were far and away most excited to hear about a skill tree and new class archetypes
+            </li>
+            <li>
+                People thought that the new update would go in a different direction (Dern when amirite) but aren’t upset about the new changes being implemented
+            </li>
+            <li>
+                Some minor things people hope are addressed in the update include the dialogue system and archer’s escape spell, but it is unclear if these changes will be included in the new update
+            </li>
+            <li>
+                The biggest concern for current players was the surge of new and returning players, more guild activity, and possibly more frequent attacks on defender guilds.
+            </li>
+            <li>
+                Additional comments in the words of RawGamingYT: “(nessak sauna sauna sauna sauna sauna sauna)”
+            </li>
+        </ol>
+        <p>
+            I of course was unable to talk to every single guild member, but if you have a specific point you want to talk about, feel free to throw it in general, or DM it to me!
+        </p>
+        <h4>
+            Member Shout of the Publication
+        </h4>
+        <p>
+            <strong>
+                ChristmasBear
+            </strong>, <em>
+                for his recent help with work in Brilliance
+            </em>
+        </p>
+        <p>
+            <strong>
+                Trivia Fact
+            </strong>: Castor, aka Beta Geminorum, is actually a gravitationally bound system of 6 stars (although we can only see one with the naked eye).
+        </p>
+        <code>
+            Closing Remarks
+        </code>
+        <p>
+            If you’ve made it this far, thank you! I am starting to have a lot of fun putting these together, so I hope you all are enjoying reading as much as I am writing. As always, if there’s something you want featured in the next edition, or you think you might want to help out in the future, send over an application to Titan’s Brilliance (imagine being in Brilliance because you know how to code, couldn’t be me and I’m definitely not the only one who’s here for creative endeavors). Again, thank you Andrew for making my work more accessible, and thank you, reader!
+        </p>
+        <h6>
+            bitesizedbee4 &lt;3
+        </h6>
+    </>);
 }
