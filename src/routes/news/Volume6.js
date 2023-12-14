6834ef2d0938650a84aaa43f4f9f3a7a40680291
FoxSylv <atheriault@bowdoin.edu>
2023-12-14 16:55:33 -0500

    Migrate Volume 6

--- src/routes/news/Volume6.js
+++ src/routes/news/Volume6.js
@@ -1,3 +1,243 @@
+import TriviaFact from '../../components/TriviaFact';
+
 export default function Volume6() {
-    return;
+    return (<>
+        <p>
+            Hey everyone and thanks for opening another edition of That One Publication No One Asked For! Or should I say Titan Times, the best place to get your fill of guild activity and thoughtful insights into all things Wynn! Some fun promotions, a good bit of trivia, and the anticipated results of the survey posted not long ago. Let’s hop in!
+        </p>
+        <h2>
+            Recent Happenings 📅
+        </h2>
+        <center>
+            <img src="https://cdn.discordapp.com/attachments/717490202909737101/1029911784682958929/2022-09-18_23.16.30.png" alt="Group of players" width="66%" />
+            <p>
+                Ice is the guy in blue dyed armor
+            </p>
+        </center>
+        <p>
+            To start, I want to acknowledge the long term hiatus of ANO’s founder, IceResistance. We’ll miss you Ice, and wish the best for you in your endeavors. Two years will go by in the blink of an eye!
+        </p>
+        <p>
+            2.0 was finally released between my last publication, and it’s been so much fun playing around with the new features (though we’ll see what the rest of the guild thinks in a bit !!). Now is a great time to start new classes and experience the game in a whole new way! I hope you all are having fun with it so far.
+        </p>
+        <p>
+            Also, The second annual Festival of the Bonfire is ongoing for the next month or so! Come check out the decorated Detlas, have a go at the Battle of the Bonfire minigame, and exclusive Bonfire crates (and Spirit crates, but they do not contain new cosmetics this year, only ones from the Festival of the Spirits last October) are on sale now! Crates can also be obtained by completing daily objectives and doing your guild objectives. There’s never been a better incentive for doing your guild objective! I always love the seasonal festivals, so I hope you get a chance to check things out!
+        </p>
+        <p>
+            There’s been quite a few events recently! BlueTheSniper has hosted a variety of minigame events including Tactical Gaggle and Spectral Zone. Additionally, Ugastag and Biblonko hosted an event to create a brand new guild advertisement to be used in the Wynncraft discord server. I had fun creating my submission, but big congratulations to 4NZ1K for creating the best advertisement!
+        </p>
+        <p>
+            If you’re interested in participating in future events, make sure you grab the role in #guild-info, or if you think you might like to help plan events in the future, consider applying to Titan’s Spirit (the tickets for which can also be found in #guild-info)!
+        </p>
+        <p>
+            There was also an event over in Adonis hosted by ESI, a Mafia style game with professions involved! Citizen players had to create certain crafted items, while the mafia members tried to avoid detection. I was unfortunately unable to attend, but many of our members made it on the leaderboard including Callum, GebutterteWurst, RawGamingYT, LaMDaKiS, and ChristmasBear!
+        </p>
+        <p>
+            There’s also an ongoing event happening in Adonis (it's finished now)! It’s a Parkour Spiral made by our very own Constellar! Lots of LE up for grabs with this event, so I hope everyone who tries it out has lots of fun.
+        </p>
+        <center>
+            <img src="https://cdn.discordapp.com/attachments/958939382235140106/1028743774676987924/unknown.png" alt="Parkour leaderboard" width="66%" />
+            <p>
+                Parkour Leaderboard
+            </p>
+        </center>
+        <div className="indented">
+            <p>
+                <strong>
+                    ScientificSnake, T1hunna, Aviararo, Seaskipper, Chinos, Garr, AWornOutShoe, 50ish, CleansingFlames
+                </strong>
+                , and <strong>
+                    Patia_
+                </strong> have all been promoted to Sorcerer!
+            </p>
+            <p>
+                <strong>
+                    ccanonn, Jk725, xXMemelord69420, PenStick
+                </strong>
+                , and <strong>
+                    _1Fps
+                </strong> have been promoted to Sorcerer!
+            </p>
+            <p>
+                <strong>
+                    Flipiik
+                </strong> has been promoted to Sorcerer!
+            </p>
+            <p>
+                <strong>
+                    Lucky32, BlueTheSniper and aFireblaze
+                </strong> <em>
+                    (wb fireblaze btw)
+                </em> have been promoted to Artificer!
+            </p>
+        </div>
+        <p>
+            Congratulations to all our recent promotees, and I hope to see some new names up here soon!
+        </p>
+        <h2>
+            Military Updates ⚔️
+        </h2>
+        <p>
+            Starting off, our Season 8 reclaim of the ocean went well! Our claim this season is very similar to that we’ve had for many seasons now, so not much to report on there. Let’s make this another great season for the books!
+        </p>
+        <p>
+            Our military alliance, Cucumber Company (Cucumber was archived. The new alliance name is called "Smtn else"), has seen a few changes back in the beginning of the season. Gang of Fools [FOOL] has left and betrayed the alliance, but we’re happy to welcome Paladins United [PUN], The Simple Ones [ILQ], and Terralune [UTL] to our wider guild family!
+        </p>
+        <p>
+            Season 5 of Ocean Trials starts. It's a seasonal event in which you can earn LE for participating in wars! You don’t have to be a certain rank or have a certain build to participate. You will, however, be required to install and use a mod for the duration of the season in order to have a valid entry. We’re trying something new this season, so there’s bound to be a few hiccups, but it should help us out immensely if all goes well. If you want more information, make sure you have the military role (if you do not and would like it, please let a council member know) and grab the Ocean Trials role from #military-info!
+        </p>
+        <p>
+            Rounding out this section, I want to congratulate Lucky32 and aFireBlaze for passing the strategist test!
+        </p>
+        <p>
+            As a reminder, any member who is currently Captain and (and Artificer) has at least 300 total wars can attempt the strategist test.
+        </p>
+        <h2>
+            Main Stage: ANO’s Thoughts on The Spellbound Update 🌎
+        </h2>
+        <p>
+            I know a lot of my publications have talked about something to do with the 2.0 update, but this is (at least most likely) the last you’ll hear about it from me! Now that the update has been out for a while for all players, I wanted to get a sense of what the members of Titans Valor thought. Most of you (unless you’re very new, in which case hi new people, hope you like it here) saw the survey I posted a few weeks ago, and I wanted to share the results here!
+        </p>
+        <p>
+            I am happy to report that I received a total of 41 responses (shame to the other approximately 50 of you who chose not to respond, luckily I kept responses anonymous so y’all are safe this time…). I’m also going to share some of my favorite responses to the last question in the survey because some were quite entertaining. Without further adieu, the results!
+        </p>
+        <div className="indented">
+            <h4>
+                What is your main class?
+            </h4>
+            <p>
+                More than half of us are playing on either Warrior or Mage (they were very close) after the update now. Seems like this is very similar to before the update, we’re still unoriginal!
+            </p>
+            <center>
+                <img src="https://cdn.discordapp.com/attachments/717490202909737101/1029913431530274826/unknown.png" alt="Class usage pie chart" width="66%" />
+            </center>
+            <h4>
+                What is your main class’s combat level?
+            </h4>
+            <p>
+                Nearly everyone has at least 1 class over 100, but a couple of you are still down in the 70-80 range. I see you and I know you’ll be there soon! Leveling has been more fun since the update, I’ve been working on a new class myself recently!
+            </p>
+            <h4>
+                What has been your favorite part of the update so far?
+            </h4>
+            <p>
+                ANO members overwhelmingly love the new skill tree and class archetypes, but also are enjoying the new Nameless Anomaly raid and the new quests. Personally, new Galleon’s Graveyard is just \*chef’s kiss\* so much better. I hope we get more improved dungeon experiences in the future now that we’ve seen what the content team is capable of.
+            </p>
+            <h4>
+                What has been your least favorite thing about the update so far?
+            </h4>
+            <p>
+                Of course bugs are always the top answer for something like this, but hopefully those will get worked out in time.
+            </p>
+            <p>
+                Lots of people mentioned some form of class rebalancing issue (whether it be the nerfs to Defense and Agility, the fact that Assassin is now pretty useless, or having to choose between high DPS or survivability.) I think the skill tree provided a good start for having varying playstyles in each class, but I’m sure we’ll see changes to it in the future. If you hate it now, hope is not lost!
+            </p>
+            <h4>
+                If there’s one thing you could change about the update, what would it be?
+            </h4>
+            <p>
+                I got a huge variety of responses here, but most of them were related to specific archetype abilities. Another thing that really stood out was nerfing the enemies in Nexus of Light and The Canyon Colossus, as these are now more difficult to complete than the endgame raid.
+            </p>
+            <h4>
+                What has been your favorite archetype to play as overall so far?
+            </h4>
+            <p>
+                As a caveat, I know lots of people use builds that cross between multiple archetypes, so I’ve asked people to select the choice that they put the most skill points into. Take the next few questions with a grain of salt!
+            </p>
+            <p>
+                Sharpshooter and Battle Monk come out on top for this one! Nearly all the archetypes were represented here (minus Trickster and Ritualist), so I’m glad to see everyone is finding something to enjoy!
+            </p>
+            <center>
+                <img src="https://cdn.discordapp.com/attachments/717490202909737101/1029914573194670111/unknown.png" alt="Archetype breakdown pie chart" width="66%" />
+            </center>
+            <h4>
+                Favorite archetype for warring?
+            </h4>
+            <p>
+                Oddly enough, most people chose to skip this question, I assume because not everyone who completed the survey are active warriors (which is fine!). For those who did answer though, Fallen was far and away the favorite.
+            </p>
+            <h4>
+                Favorite archetype for raids?
+            </h4>
+            <p>
+                Paladin and Sharpshooter were both very close! Again, lots of people chose to skip this one. Just goes to show how differently each person enjoys this game!
+            </p>
+            <h4>
+                Thoughts on how the update has impacted the marketplace economy?
+            </h4>
+            <p>
+                Most of the answers here were about the insane inflation we’ve been seeing lately, and hopes that this will calm down in a few months.I’m definitely seconding that! (mama needs an idol)
+            </p>
+            <h4>
+                How would you rate the update on a scale of 1-10?
+            </h4>
+            <p>
+                Most of our respondents rated this update between a 7 and 9, which is pretty good! In the follow up question, players mentioned needing a lot of bug fixes and other QOL fixes, but also doing better class rebalancing.
+            </p>
+            <h4>
+                And now the fun part…the best responses!
+            </h4>
+            <p>
+                “ANO is a good guild”
+            </p>
+            <p>
+                I agree
+            </p>
+            <p>
+                “give me all your stx, this is a threat”
+            </p>
+            <p>
+                It ain’t much but also no
+            </p>
+            <p>
+                “I love what you do here with the Titan Times! It's one of the top reasons I love this guild, and you always blow my mind with how much effort is put into it. :)”
+            </p>
+            <p>
+                Thanks! I try
+            </p>
+            <p>
+                “Cal smells”
+            </p>
+            <p>
+                On god
+            </p>
+            <p>
+                “Wanna kiss?”
+            </p>
+            <p>
+                Only if you ask nicely
+            </p>
+        </div>
+        <p>
+            There were lots of good responses here, y’all really pulled through. Thanks for the laughs (and the multiple links to rickrolls, y’all can’t outsmart me). I hope you found this interesting, I thought it was a good overview of how our guild members feel about the new update. Happy playing  everyone!
+        </p>
+        <h4>
+            Member Shout of the Publication
+        </h4>
+        <p>
+            <strong>
+                RawGamingYT
+            </strong>, for Bee appreciation post
+        </p>
+        <p>
+            <strong>
+                Trivia Fact
+            </strong>
+        </p>
+        <TriviaFact>
+            Since I started writing this before losing power from Hurricane Ian, here’s a hurricane fact: the deadliest Atlantic Hurricane on record is Hurricane Mitch, which devastated Central America in 1998, costing an estimated 11,000 people to lose their lives.
+        </TriviaFact>
+        <h3>
+            Closing Remarks
+        </h3>
+        <p>
+            If you made it this far, thank you for reading! I know these closings feel pretty similar each time, but I always want to reiterate how much your support means to me and keeps me motivated to keep writing for all of you. I know my publication schedule isn’t very consistent, but I try to always make sure I have a good Main Stage segment, which is usually the main reason for delay. If you ever have ideas, feel free to DM me! Or if you think you’d like to help work on the Times in the future, consider submitting an application to Titan’s Brilliance.
+        </p>
+        <p>
+            Once again, thank you all, and I hope you’re all well. I send good vibes.
+        </p>
+        <h6>
+            bitesizedbee4 &lt;3
+        </h6>
+    </>);
 }
