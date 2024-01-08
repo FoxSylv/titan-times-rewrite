99ea1ffe385ce27d32a096e8a7bc614ff57010d5
FoxSylv <atheriault@bowdoin.edu>
2024-01-08 01:47:47 -0500

    Migrate Volume 11
    That's all of them!!!

--- src/routes/news/Volume11.js
+++ src/routes/news/Volume11.js
@@ -1,3 +1,194 @@
+import TriviaFact from '../../components/TriviaFact';
+
 export default function Volume11() {
-    return;
+    return (<>
+        <p>
+            Hello once again Titans, welcome back to the Titan Times: the best place to get your fill on guild activity and insights on all things Wynn! A bit of a shorter one this month, as this time of year tends to take many of us away from our favorite block game, but I think you’ll still enjoy some great event updates, some friendly reminders, and another great contribution from featured writer, karapandza! Where one quest fails, another finds success. You’ll have to keep reading to find out which one I mean, so let’s work our way there, shall we?
+        </p>
+        <h2>
+            Recent Happenings 📅
+        </h2>
+        <p>
+            I hope you all enjoyed that DXP weekend we had just a few weeks ago (Cal called it, much to our collective dismay). Wynncraft’s 10th anniversary has finally been celebrated to its fullest. How many people picked up a commemorative Orange Wybel plushie? If you still haven’t made up your mind, they’re still available for a very short time (as of writing, the window may now be closed). Get one while you still can!
+        </p>
+        <p>
+            Our guild housing island has now surpassed 2000 upvotes! Thank you to everyone who has worked on this massive project so far; it seems your efforts have not gone unnoticed.
+        </p>
+        <center>
+            <img src="https://cdn.discordapp.com/attachments/703019773965107250/1111498782086017115/2k.jpg" width="66%" alt="2000 upvotes" />
+            <p>
+                Original image was definitely not 2001 upvotes. 100% not photoshopped by Asoart.
+            </p>
+        </center>
+        <p>
+            We also selected some winners for the LE giveaway graciously hosted by ANO veteran, T1Hunna. Congrats to winners <strong>
+                Ethereal
+            </strong> (real), <strong>
+                LevelDown, Snazz, and Biblonko
+            </strong>
+            , you all get a little extra money in your pockets (I’m not yet convinced Bib didn’t rig the results in his favor).
+        </p>
+        <p>
+            We round out our recent events with my favorite part–promotions! There are lots of new Sorcerers this month, so help give congratulations to <strong>
+                xkowota, KevinBoiii, sulguni, Exstern, Zaelot976, Iamgenerous, LevelDown, <em>
+                    Admiral_Kiwi
+                </em>
+                , BladeofDeath, Zeyka, DragonIsPro, ThatCheater, milandp, SFChen, IApprove, Cosart, JeHooft
+            </strong>
+            , and <strong>
+                Axlod
+            </strong>
+            ! Your presence in the guild has not gone unnoticed.
+        </p>
+        <h3>
+            What’s in the Cupboard?: ANO Cabinet Review
+        </h3>
+        <h4>
+            Spirits
+        </h4>
+        <p>
+            <strong>
+                Micah
+            </strong> made a return to the events scene by hosting Minecraft minigame Missile Wars! The gameplay is derived pretty easily from the name, but in case it wasn’t obvious: teams launch rockets at an opposing team’s portal. The event was attended and enjoyed by many, so thank you Micah for another great event!
+        </p>
+        <p>
+            <strong>
+                Biblonko
+            </strong> and <strong>
+                Ugastag
+            </strong> came through with another installment in their series of Hide and Seek events. This one had participants hiding in the sprawling Karoc Quarry. Congrats to our champion hiders <strong>
+                kebabdoner, Aemor, IApprove
+            </strong>, and <strong>
+                Marraino
+            </strong>
+            , who are walking away a tad richer for their skills!
+        </p>
+        <center>
+            <img src="https://cdn.discordapp.com/attachments/703019773965107250/1111494140706967572/e977cad4-73d6-4836-b1e2-a90d4a082184.png" width="66%" alt="Karoc Quarry hide-and-seek" />
+        </center>
+        <p>
+            OverFears attempted to host a Wynncraft Bingo event, but unforeseen technical problems have delayed the festivities. The new date and time are coming up very soon (or perhaps already passed by the time this comes out), so make sure to continue looking at #events for updates if you’re interested in participating. These are a fun way to get to know people with a potential prize in store if you do well, so I highly encourage our newer members to give it a try!
+        </p>
+        <p>
+            Also coming up soon, [ATVA] is hosting a Wynncraft Scavenger Hunt that many guilds will be participating in! This is a great way to hang out with people outside of our guild for a change, and there’s a rapidly increasing prize pool to be had by the winners. A separate server has been created to contain all the event details, so if this sounds like something you might want to participate in, the server invite has been posted in #events. Good luck everyone!
+        </p>
+        <div className="indented">
+            <p>
+            Hey you! Yes, you there! Does hosting your own event sound like something that interests you? Or maybe you want to collaborate with someone on a new event? Consider applying to Titan’s Spirit today! This is the subsection of our members that plan and host all the events we get to participate in. As a side benefit, participating in a cabinet is more likely to get you promoted in the future. ALL CURRENT MEMBERS are eligible to apply, so there’s nothing holding you back except your imagination!
+            </p>
+        </div>
+        <h4>
+            Brilliance
+        </h4>
+        <p>
+            Our Brilliance members have had a bit of a lull this month, but we can still get excited for what has been going on in the background (besides this newspaper of course :D )
+        </p>
+        <p>
+            A few new commands were added to the bot though. You can use the <code>
+                -guildgroup
+            </code> command to group guilds together for use in <code>
+                -alliance stats
+            </code> or <code>
+                plot2
+            </code> (still need to add this in later) etc. instead of typing out a long list of guild tags.
+        </p>
+        <p>
+            Other conveniences such as <code>
+                -season
+            </code> were added to help with <code>
+                -r range
+            </code> queries so you don't need to compute the days offset every time you want to find seasonal data.
+        </p>
+        <p>
+            Additionally, we overhauled our war tracking web server to be written in Rust and using the main SQL database.
+        </p>
+        <p>
+            The builders amongst us have once again been progressing on our guild housing. The inside of the castle is no longer being neglected–for the moment anyway.
+        </p>
+        <center>
+            <img src="https://cdn.discordapp.com/attachments/703019773965107250/1111494142091087932/4c41a04f-0f14-463d-a349-3341cd6823ea.png" width="66%" alt="Undeveloped castle interior"/>
+            <p>
+                The Backrooms 2.0
+            </p>
+            <img src="https://cdn.discordapp.com/attachments/703019773965107250/1111494142913171516/4f9caa98-a07d-49b0-9e2d-cb1f4fda876e.png" width="66%" alt="Undeveloped castle interior" />
+            <p>
+                Cool banners
+            </p>
+            <img src="https://cdn.discordapp.com/attachments/703019773965107250/1111494141348683846/b35deb56-f358-4f6f-a300-ff3b252956b2.png" width="66%" alt="Castle entrance" />
+            <p>
+                Looks pretty good from out here
+            </p>
+        </center>
+        <div className="indented">
+            <p>
+                We’re always looking for new members too! Titan’s Brilliance covers a range of skills from coding, building on the guild housing, creating artwork, writing for the very newsletter you’re reading now, or any other creative endeavor you can think of! There’s LE in store for everything you complete as added incentive. If any of these are skills you have, consider submitting an application today! Applications are open to ALL CURRENT MEMBERS, and participation in a cabinet can help you get promoted in the future!
+            </p>
+        </div>
+        <h3>
+            Military Updates ⚔️
+        </h3>
+        <p>
+        The end of Season 12 is fast approaching! Anyone participating in Ocean Trials this season had better start racking up those FFA wars if you want some of that sweet, sweet LE.
+        </p>
+        <div className="indented">
+            <p>
+                A quick note for Ocean Trials participants: Anode, the mod we use to track wars, has received an update. If you haven’t already done so, please make sure you update to the newer version if you want to continue participating in Ocean Trials. Wars completed not using Anode will not count towards your end of season total.
+            </p>
+        </div>
+        <p>
+            There’s also been a change to the map mid-season. While this doesn’t affect our claim at all, it does affect the claim of our allies. Anyone interested in ranking up by warring should pay attention to any future map changes, especially those interested in learning eco.
+        </p>
+        <p>
+            Good news on the organization side of things: our list of war builds is migrating and evolving! Get excited! We now have a forum in the Military section of the Discord that will be the future home of all our current war builds. The hope is that not only will they be updated more regularly as the meta continues to change over time, but also provide better instructions for each build to those of you who are learning to war for the first time. Much more user friendly than the old war builds document! If you have questions about the new system or about the builds themselves, feel free to ask questions in <code>
+                #military-chat
+            </code>
+        </p>
+        <h2>
+            Main Stage: The Breaking Point Reviewed 🪨
+        </h2>
+        <blockquote>
+            Written by karapandza
+        </blockquote>
+        <p>
+            After looking at why both A Journey Beyond and a Journey Further fail to serve their purpose, I want to look at how The Breaking Point not only fulfills what it set out to do, but also excels in many ways and thus has become my personal favorite Wynncraft quest. This piece will be a bit shorter since I do not have as many thoughts surrounding this quest but nonetheless, it is still interesting to analyze.
+        </p>
+        <p>
+            There are 2 factors that lead to this: gameplay and story/dialogue, and how these interact with the player. I also want to preface all of this with the fact that The Breaking Point is one of the newest quests and of course can take advantage of all the knowledge and the skills that the Wynncraft team has developed over these many years.
+        </p>
+        <p>
+            The gameplay in this quest is simply brilliant. Each section is fun and memorable. Oftentimes quests end up just consisting of the player getting resources and bringing them to an NPC. Even though this quest has many such moments they are surrounded by interesting and creative parkour and fun combat. I never got bored whilst playing; every section clicked with me. None of the gameplay is repetitive and this can be attributed to the smart use of teleportation. Even though I did not touch on this in my last piece, the multiple and rapid use of teleports in A Journey Beyond helped the quest feel even less like a proper adventure, which it should strive to be. I am not against the use of teleportation in quests, but it must be done intelligently to properly fit in. Of all the quests that I have done, The Breaking Point utilizes teleportation and fast travel perfectly. It is used at the right intervals where it does not feel overused, and within the context of the quest, it makes perfect sense (I would like to add that Aledar's use of teleportation also makes sense but my issue with it is that is simply used too often for such short distances). The gameplay is fun, creative, and engaging. This is what all quests should aim to achieve.
+        </p>
+        <p>
+            What I love most about his quest is how it makes everything you have done feel even more important. The dragon you once got out of the vaults of Thanos led to something larger. Even the two Corkus quests led to something larger. But it also gives closure. All your actions in the Dwarves and Doguns quest line really meant something. You as the player caused change for the better. You affected these people. Often the continuity of Wynncraft to me at least is forgotten but especially here you are reminded that time is always passing and the lives of the people you saved are advancing. I cannot praise this quest enough for how it highlights all the great tasks the player achieved by then. Since the addition of the multiple-choice dialogue system, I was personally never a huge fan of it. But once again this quest uses it brilliantly. The Player is so involved in the dialogue whereas in many other quests you end up feeling like a spectator, only partially being involved in what is being discussed. The story once again is fun and leads you to different diverse places. From the icy caves of Molten Heights to a small research lab on the island of Corkus. Watching different NPCs from such different quests and places interact is so enjoyable. It displays that Wynn is a living place with people who need help from others from kilometers away.
+        </p>
+        <p>
+            I love this quest. It takes full advantage of everything Wynncraft has to offer today. It connects people that are so far apart because of what the player did. It highlights the importance of all the actions one has taken thus far. And it makes Wynncraft feel alive. I love this quest and I cannot wait for what quests await us in Fruma!
+        </p>
+        <h3>
+            Member Shout of the Publication
+        </h3>
+        <p>
+            <strong>
+                DannYTop123
+            </strong>
+            , for notable activity lately.
+        </p>
+        <p>
+            <strong>
+                Trivia Fact
+            </strong>
+        </p>
+        <TriviaFact>
+            Cats have front paw dominance, similar to how humans have handedness. Male cats are more likely to be left-pawed, while female cats are more likely to be right-pawed.
+        </TriviaFact>
+        <h3>
+            Closing Remarks
+        </h3>
+        <p>
+            If you made it this far, thank you! Writing this piece was something I kept putting off because I’ve been busy with IRL things and my free time is mostly spent decompressing from that. It leaves me with not very much creative energy at the end of the day. But we got it done! A very special thank you to karapandza for another great article. The idea to do a direct comparison from his last submission was so well thought out, so make sure you give him some extra praise. And of course thanks to Andrew for making sure this gets posted where it’s supposed to, plus all my fellow Brilliance members who help review these articles before the final publication. Hope to catch you all in the next one!
+        </p>
+        <h6>
+            &lt;3 bitesizedbee4
+        </h6>
+    </>);
 }
