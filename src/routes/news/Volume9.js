f58361eb43e8e9dfa394d7c647ecc0169368697f
FoxSylv <atheriault@bowdoin.edu>
2024-01-08 00:38:17 -0500

    Migrate volume 9

--- src/routes/news/Volume9.js
+++ src/routes/news/Volume9.js
@@ -1,3 +1,255 @@
+import TriviaFact from '../../components/TriviaFact';
+
 export default function Volume9() {
-    return;
+    return (<>
+        <p>
+            Hello again Titans, welcome to the celebratory volume of the Titan Times: the best place to get your fill on guild activity and insights on all things Wynn! Today I get to be the messenger of many bits of good news. We’ve got lots of promotions to go over, an update from the guild housing, and some big news about ANO to talk about later. Put your hands together for all of our achievements this past month, and let’s get into it.
+        </p>
+        <h2>
+            Recent Happenings 📅
+        </h2>
+        <p>
+            First on the docket is a reminder that our Guild Housing recently hit <strong>
+                1000 upvotes
+            </strong>
+            ! Our guild housing was the first (and, at the time of writing, only) public housing plot to do so. Our builders have been hard at work carrying out their magnificent plans, so if you haven’t seen it recently, make sure you check it out.
+        </p>
+        <blockquote>
+            As a side note, the guild housing is not yet complete. If you think you’ve got the building skills and are interested in helping work on the project, be sure to ask a council member!
+        </blockquote>
+        <p>
+            I’ll tease it now even though I’ll be talking about this more in a bit, but congrats to <strong>
+                Geb and Aemor
+            </strong> on our first two GXP X awards. This is awarded to members who donate over 100 billion XP to the guild (though they’ve both done considerably more than that). They, amongst others, have been hard at work lately!
+        </p>
+        <blockquote>
+            editor's note: btw, the auto gxp role feature is working now. You can grind xp and automatically have the corresponding role upon reaching the threshold.
+        </blockquote>
+        <p>
+            On to my favorite part of the publication, promotions! I’m happy to announce there’s a lot up here this time.
+        </p>
+        <div className="indented">
+            <p>
+                <strong>
+                    Bynt, TM20, ChaoticGlitched, Trekkie_Cow124, LittleFriend_, Nasssss, DungeonMerchant, Jurrior, karapandza, Koenig_Michi, Avidlearn3r, jDog116, kisseymiyake, Rudolf2305, nicholxs, hodaga, xTimi_, Magireal, Kenturion12, miniPCpro, KidOfCubes, and OverFears
+                </strong> have all been promoted to Sorcerer! Good job proving your dedication to the guild and beginning to make your way up the ladder.
+            </p>
+            <p>
+                <strong>
+                    kebabdoner, BeGruent, and BlueTheSniper
+                </strong> have all been promoted to Artificer! We see all the hard work you’ve been putting into your respective areas.
+            </p>
+            <p>
+                Make sure to congratulate our newest Titan, <strong>
+                    Aemor
+                </strong>
+                ! His hard work grinding GXP and help with warring has been much appreciated.
+            </p>
+            <p>
+                Finally, huge congratulations to <strong>
+                    Asoart
+                </strong>
+                , the newest addition to the Sage Council! His extensive war and eco knowledge, plus being active in the community has put his name as one synonymous with the guild. Hope you’re loving all the new responsibilities!
+            </p>
+        </div>
+        <p>
+            Congrats to all of our promotees! I can’t wait to see new names up here soon.
+        </p>
+        <h3>
+            What’s in the Cupboard?: ANO Cabinet Review
+        </h3>
+        <h4>
+            Spirits
+        </h4>
+        <p>
+            You may remember last time that we had an ongoing photography contest hosted by <strong>
+                BeGruent
+            </strong>
+            . Since then, the event has ended and the judges have spoken. Congratulations to <strong>
+                Viirns
+            </strong> for his gorgeous photo! <strong>
+                _EliteDreamer
+            </strong> was the notable runner-up. I did tell you all that nice shaders would get you brownie points, no?
+        </p>
+        <center>
+            <img src="https://cdn.discordapp.com/attachments/703019604968210482/1089367022074331317/58fad6b5-23e9-4b06-ae24-3c5f27a7be4f.png" width="66%" alt="Light Forest with shaders" />
+            <p>
+                Viirns' Winning Capture
+            </p>
+        </center>
+        <p>
+            Over on the Adonis server, our friends from [AVO] hosted a couple of casual art games: skribble.io and Gartic Phone. The former is similar to an online version of Pictionary, where one player is given a word or phrase to draw and everyone else tries to guess what it is. Gartic Phone takes this one step further by adding in rules from the classic game of Telephone. In this game, you begin by writing a word or phrase for someone else to draw, and you then are randomly and anonymously given someone else’s phrase to draw. Players then pass their drawings along again, and you must create a new phrase that describes the drawing. At the end of the round, everyone gets to see how the original phrase progressed. All said and done, hilarity generally ensues. These are low skill, casual games frequently hosted by our events team, so make sure to join the next time someone hosts!
+        </p>
+        <div className="indented">
+            <p>
+                Now some of our newer members might be asking “hey, what’s Adonis?” Great question recruit! We are part of a community alliance with [AVO] and [ESI] called Adonis. This gives players from different guilds a chance to interact and have more fun together. If you’re not in the server and would like to be, an invite link can be found in #guild-info.
+            </p>
+        </div>
+        <p>
+            Also in Adonis, [ESI] has just wrapped up their item design competition. Participants were asked to imagine a brand new item for Wynncraft across any level range (or even beyond the current level cap). There were several different categories, including best art, best meme, and many more. ANO members swept this competition with their submissions, taking 4 out of 5 of the top spots. Congrats to <strong>
+                Acrohh, LaMDaKiS, BeGruent, and Silent__Boss
+            </strong> for all of their creative designs!
+        </p>
+        <center>
+            <img src="https://cdn.discordapp.com/attachments/703019604968210482/1089367024574140527/ea5686a5-036f-4898-81fc-7a56f1dd8910.png" width="66%" alt="Custom item 'Stand'" />
+            <p>
+                It's a JoJo reference?
+            </p>
+        </center>
+        <p>
+            Our Llama Councilors, Ugastag and Biblonko hosted a Wynncraft Hide-and-Seek event. This time, participants were confined to the city of Ahmsord in the first half, while the second half was played in the Ruined Olmic City. <strong>
+                kebabdoner, 28culture, Dr_san, _Constellar, Asoart, LaMDaKiS, and GebutterteWurst
+            </strong> all were able to win some rounds (and some LE)!
+        </p>
+        <p>
+            They also debuted a new event over in Adonis: Copycat! Players were given a screenshot of an inventory with 23 items to recreate (sprite-wise) as best they could in the time given. To add to the challenge, all players had to use a new Ironman class. Congratulations to <strong>
+                kebabdoner
+            </strong>, who took first place with 21 items matched!
+        </p>
+        <p>
+            <strong>
+                Asoart
+            </strong> tried his hand at hosting his first event: Wynncraft Trivia! Featuring 3 different categories of questions–Wynncraft knowledge, Minecraft knowledge, and general trivial–the first person to answer the question correctly in guild chat was awarded a point for the round. <strong>
+                Cal
+            </strong> was able to best everyone with his trivia knowledge, earning himself a total of 20 points. Honorable mentions are <strong>
+                Geb
+            </strong> with 18 points, <strong>
+                CloudXYZ15
+            </strong> with 10 points, and <strong>
+                Aemor
+            </strong> with 8 points. Great job everyone that earned themselves some LE!
+        </p>
+        <p>
+            Speaking of <strong>
+                Aemor
+            </strong>
+            , he also hosted his first event this month: the “ANO Gets to 100” GXP grind event! Participants were awarded raffle tickets for some great prizes depending on the amount of XP they earned. Incentivizing the grind meant that we were able to smash our goal, going from level 97 to 100 in just a few days. Thank you to everyone who participated! Hopefully some of you are enjoying some nice raffle prizes now.
+        </p>
+        <p>
+            Rounding out the Spirit recap is <strong>
+                BlueTheSniper
+            </strong>
+            , who routinely hosts fun party games, such as Spectral Zone, Bolt, and others. Most recently, he hosted Gartic Phone on our server, but just before that he hosted Kitchen Burnout over in Blue’s Builds.
+        </p>
+        <blockquote>
+            That’s right, it’s time for the portion of the publication where I shamelessly promote Blue’s Builds yet again. Blue owns a server that gathers and updates one of the largest collections of Wynncraft builds. If you can think of a build you might want, chances are you can find it already made over on Blue’s Builds. Make sure you head over there if this sounds like something you might be interested in, or you just don’t want to miss out on more fun events! An invite link can be found on his Discord profile.
+        </blockquote>
+        <p>
+            A quick reminder that if you’re interested in creating and hosting guild events in the future, be sure to submit an application to Titan’s Spirit today!
+        </p>
+        <h4>
+            Brilliance
+        </h4>
+        <p>
+            A much shorter section than the Spirit updates, but hey, Brilliance projects take time to complete. Rest assured, something is always going on quietly.
+        </p>
+        <p>
+            _Constellar is in the process of giving our guild uniforms a facelift. While there isn’t a timeline for when these might be ready for all of you to wear yourselves, be sure to keep your eyes peeled if you’re looking for some new clothes.
+        </p>
+        <p>
+            Something I’m quite excited about is <strong>
+                Snazz
+            </strong> working on automating some of our Discord roles, such as the GXP and Wars. This will make it much easier for all of you to show off some of the ways in which you contribute to the guild. How neat is that?
+        </p>
+        <blockquote>
+            editor's note: thanks Raw for fixing Snazz's code.
+        </blockquote>
+        <p>
+            And of course, our Times team is always up to something…
+        </p>
+        <p>
+            If you’ve got coding or creative skills and want to help the guild out in some way, make sure you submit your application for Titan’s Brilliance (to sweeten the deal even further, each task you’re able to complete will earn you some LE 😉)!
+        </p>
+        <h3>
+            Military Updates ⚔️
+        </h3>
+        <p>
+            We’re halfway through Season 11, which still leaves plenty of time to get those wars in for Ocean Trials (or meet the necessary war count if you’re looking to apply for captain or strategist).
+        </p>
+        <p>
+            While we’re on the topic of Ocean Trials, we have a new rewards system in place now! Rewards are distributed on a weekly basis throughout the season, rather than getting a lump sum at the end. Each week, you’ll be given 1 LE for every 15 wars you participate in. During the last week of the season, all rewards will DOUBLE as extra incentive to get a final push up for our season ratings. There are also additional bonuses at the end of the season for whoever gets the most wars overall (and of course the exclusive Champion of the Ocean role in Discord, but this part is not new). As a reminder, if you want to participate in Ocean Trials, you must have ANOmod installed in order to track your wars. Good luck warrers!
+        </p>
+        <p>
+            We have some exciting war-related promotions to announce down here! <strong>
+                Flipiik, Endriik, and kebabdoner
+            </strong> have all passed the strategist test. This requires a good amount of knowledge about economy and guild politics, so huge congrats to them. Additionally, <strong>
+                aFireBlaze
+            </strong> was promoted to chief for his invaluable work on eco. Most of NA eco relies on him, so this promotion is more than well-deserved.
+        </p>
+        <p>
+            In a surprise turn of events, [fuy] joined the map-holding alliance mid-season, while [NFR] has left on account of the guild disbanding. Here’s a breakdown of the territory changes:
+        </p>
+        <ul>
+            <li>
+                [fuy] has taken over most of [NFR]’s claim, with the exception of Canyon Waterfall North, Bandit Cave Upper, Burning Airship, Cinfras Thanos Transition, Cinfras County Mid-Upper, and Cinfras County Mid-Lower. These territories are now a part of the Light Forest FFA.
+            </li>
+            <li>
+                [UTL] has gained Path to Ozoth’s Spire Mid
+            </li>
+            <li>
+                [ICo] has gained Bandit Camp Exit
+            </li>
+        </ul>
+        <p>
+            This doesn’t affect us a whole lot, but we have a bunch of new territories in our popular FFA spot. Great for those wanting to get their war counts up!
+        </p>
+        <p>
+            And that about does it for our military updates! If you don’t currently have the military role and would like it, be sure to ask any Titan+. Also, don’t be afraid to ask questions in <code>
+                #military-chat
+            </code>!
+        </p>
+        <h2>
+            Main Stage: ANO Reaches Level 💯
+        </h2>
+        <p>
+            A short and sweet segment this go around, but one no less monumental for it. Titans Valor has finally reached level 100, a goal 7 years in the making! I wanted to take the opportunity to highlight the sense of pride and accomplishment we’re taking (or should be at least) at finally reaching this goal. It was a dream that ANO founder and previous owner, IceResistance, held for all of us. On his eventual return he can be proud that we’ve picked up where he left off and reached his dream for him.
+        </p>
+        <p>
+            Aemor played a huge role for us in getting to this point. He is one of the top GXP grinders in ANO history, but if it weren’t for him organizing and hosting the GXP grind event, it would have been much longer before we were able to reach this momentous goal. Thanks to him and a select few others, we were able to plow through 3 levels in a very short time. Make sure you send a little extra praise his way, he more than deserves it. Here’s a handful of pictures taken in celebration just after we reached 100!
+        </p>
+        <center>
+            <img src="https://cdn.discordapp.com/attachments/703019604968210482/1089367023483621517/dbe09337-e1be-41af-b287-0ae9226e2b9a.png" width="66%" alt="ANO photo" />
+            <img src="https://cdn.discordapp.com/attachments/703019604968210482/1089367022762213386/3b873854-6e53-4a2b-9369-32bb4f1074f8.png" width="66%" alt="ANO photo" />
+            <img src="https://cdn.discordapp.com/attachments/703019604968210482/1089367021017382932/4537ae1a-d29e-412e-bc83-fc77347de741.png" width="66%" alt="ANO photo" />
+            <img src="https://cdn.discordapp.com/attachments/703019604968210482/1089367024033087488/c9a7f957-6b91-46f8-97aa-e7096b7c08c9.png" width="66%" alt="ANO photo" />
+            <img src="https://cdn.discordapp.com/attachments/703019604968210482/1089367020581158962/1630d23d-653f-4e7f-825c-e87383d54822.png" width="66%" alt="ANO photo" />
+        </center>
+        <p>
+            To celebrate this momentous occasion, I will be hosting an Item Bomb Party and throwing <strong>
+                100 Item Bombs
+            </strong>
+            ! The date is TBA, but I will keep you all updated in coming days either in #guild-announcements or #events. Thank you to everyone who contributed to this amazing goal! Congratulations to us for being the 7th guild to ever reach 100.
+        </p>
+        <h3>
+            Member Shout of the Publication
+        </h3>
+        <p>
+            <strong>
+                _Constellar
+            </strong> Be sure to check out the Lazy Llama Cafe (thank you for the tour!)
+        </p>
+        <p>
+            <strong>
+                Trivia Fact
+            </strong>
+        </p>
+        <TriviaFact>
+            The Swedish village of Ytterby has 4 different elements on the periodic table named for it: yttrium, terbium, erbium, and ytterbium.
+        </TriviaFact>
+        <h3>
+            Closing Remarks
+        </h3>
+        <p>
+            If you made it this far, thank you. The continued support for the Times from all of you is so much of what inspires me to continue writing it. I’m always thrilled to share my work with all of you and am humbled to be able to showcase all of the cool things our community does. Special thanks to Cal for taking and sharing the level 100 celebration pictures, and thank you Andrew, as always, for doing a better job formatting this for the website than I ever could. The times might look very different without him!
+        </p>
+        <p>
+            We’ve got something special planned for next time, so stay tuned…
+        </p>
+        <p>
+            Anyways, thanks again, and I’ll catch y’all in the next one.
+        </p>
+        <h6>
+            &lt;3 bitesizedbee4 
+        </h6>
+    </>);
 }
